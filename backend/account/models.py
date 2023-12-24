c29df2c6acd1ded6bd6f2883eadabf4649d28f42
brnooliveira <brno.oliveiras@gmail.com>
2023-12-24 12:40:13 -0300

    ajustes

---
+++ backend/account/models.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(
+        User,
+        related_name='userprofile',
+        on_delete=models.CASCADE
+    )
+    resume = models.FileField(
+        null=True
+    )
---
+++ backend/account/views.py
@@ -0,0 +1,44 @@
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+from .serializers import SignUpSerializer, UserSerializer
+
+
+@api_view(['POST'])
+def register(request):
+    data = request.data
+    user = SignUpSerializer(data=data)
+
+    if user.is_valid():
+        if not User.objects.filter(username=data['email']).exists():
+            user = User.objects.create(
+                first_name=data['first_name'],
+                last_name=data['last_name'],
+                username=data['email'],
+                email=data['email'],
+                password=make_password(data['password'])
+            )
+            return Response({
+                'error': 'User registered'
+            }, status=status.HTTP_201_CREATED)
+        else:
+            return Response({
+                'error': 'User already exists!'
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+    else:
+        return Response(user.errors)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def current_user(request):
+    user = UserSerializer(request.user)
+
+    return Response(
+        user.data
+    )
